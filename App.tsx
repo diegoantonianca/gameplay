bd36d0823b342b13e98767501604ad2281964b6d
Diego Antoniança <diego.antonianca.ext@espm.br>
2021-06-26 17:01:06 -0300

    Primeiro Commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  return(
+    <View>
+      <Text> Diego Antoniança da Silva </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
